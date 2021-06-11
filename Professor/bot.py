f516cb03140c89b4c89b1a92ea1c0f0c49c13987
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-06-11 16:17:46 +0300

    1.1

--- Professor/bot.py
+++ Professor/bot.py
@@ -3,10 +3,8 @@ import os
 from discord import Intents
 from discord.ext import commands
 from dislash import slash_commands
-from boto.s3.connection import S3Connection
 
 
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 bot = commands.Bot(command_prefix="!", intents=Intents.all())
 slash = slash_commands.SlashClient(bot)
 
