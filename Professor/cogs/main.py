d4c5cb77e0e3f290eaa023a1aa8d14daea1d8517
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-06-11 20:14:38 +0300

    1.1

--- Professor/cogs/main.py
+++ Professor/cogs/main.py
@@ -1,5 +1,6 @@
 from dislash import *
 from discord.ext import commands
+from discord import Game
 
 
 class mycog(commands.Cog):
@@ -9,6 +10,10 @@ class mycog(commands.Cog):
     @commands.Cog.listener()
     async def on_slash_command_error(self, ctx, ex):
         raise ex
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity=discord.Game(name="https://github.com/SayHelloRoman/Professor"))
 
 
 def setup(bot):
